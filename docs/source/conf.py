802f3a4b97ceb5fd5ea4b28213524936f70f8908
Maddox Albro <74218483+maxer4192@users.noreply.github.com>
2022-06-08 20:05:06 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,8 +2,8 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
+project = 'ReapeR'
+copyright = '2022, Vaprix Group Limited & ReapeR Team'
 author = 'Graziella'
 
 release = '0.1'
